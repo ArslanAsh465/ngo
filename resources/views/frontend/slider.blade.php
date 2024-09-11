494b720feea86a8325c0c5a5a37dc291b50e89fc
Muhammad Arslan <ma0305422@gmail.com>
2024-09-11 12:44:57 +0500

    slider images update

--- resources/views/frontend/slider.blade.php
+++ resources/views/frontend/slider.blade.php
@@ -2,15 +2,15 @@
     <div class="slides-wrap">
         <div class="owl-carousel owl-theme">
             <!-- Slide 1 -->
-            <div class="owl-slide d-flex align-items-center cover" style="background-image: url('{{ asset('app-assets/frontend/assets/images/slider/slider_home_first_1.jpg') }}');">
+            <div class="owl-slide d-flex align-items-center cover" style="background-image: url('{{ asset('app-assets/frontend/assets/images/slider/slider1.jpg') }}');">
                 <div class="container">
                     <div class="row justify-content-center justify-content-md-start no-gutters">
                         <div class="col-10 col-md-6 static">
                             <div class="owl-slide-text">
-                                <h3 class="owl-slide-animated owl-slide-title">Empowering Change</h3>
-                                <h1 class="owl-slide-animated owl-slide-subtitle">For A Better Tomorrow</h1>
+                                <h3 class="owl-slide-animated owl-slide-title bg-white text-dark p-3">WELCOME TO AL-MUDASSAR TRUST</h3>
+                                <h3 class="owl-slide-animated owl-slide-subtitle bg-dark p-3">A SPECIAL SCHOOL FOR SPECIAL CHILDREN. A UNIQUE SCHOOL IN THE HEART OF THE RURAL PUNJAB FOR THOSE THAT SOCIETY FORGETS</h3>
                                 <div class="owl-slide-animated owl-slide-cta">
-                                    <a class="btn btn-default me-3" href="#" role="button">Get Involved Today</a>
+                                    <a class="btn btn-default me-3" href="{{ route('about_us') }}" role="button">MORE DETAILS</a>
                                 </div>
                             </div>
                         </div>
@@ -19,15 +19,15 @@
             </div>
 
             <!-- Slide 2 -->
-            <div class="owl-slide d-flex align-items-center cover" style="background-image: url('{{ asset('app-assets/frontend/assets/images/slider/slider_home_first_2.jpg') }}');">
+            <div class="owl-slide d-flex align-items-center cover" style="background-image: url('{{ asset('app-assets/frontend/assets/images/slider/slider2.jpg') }}');">
                 <div class="container">
                     <div class="row justify-content-center justify-content-md-start no-gutters">
                         <div class="col-10 col-md-6 static">
                             <div class="owl-slide-text">
-                                <h3 class="owl-slide-animated owl-slide-title">Building Futures</h3>
-                                <h1 class="owl-slide-animated owl-slide-subtitle">Together With You</h1>
+                                <h3 class="owl-slide-animated owl-slide-title bg-white text-dark p-3">IT DOESN’T MATTER IF YOU CANT SEE</h3>
+                                <h3 class="owl-slide-animated owl-slide-subtitle bg-dark p-3">ALMUDASSAR SCHOOL FOR BLIND HAS ANOTHER 50 STUDENTS LIKE YOU</h3>
                                 <div class="owl-slide-animated owl-slide-cta">
-                                    <a class="btn btn-default me-3" href="#" role="button">Support Our Mission</a>
+                                    <a class="btn btn-default me-3" href="{{ route('school_for_blind') }}" role="button">MORE DETAILS</a>
                                 </div>
                             </div>
                         </div>
@@ -36,15 +36,49 @@
             </div>
 
             <!-- Slide 3 -->
-            <div class="owl-slide d-flex align-items-center cover" style="background-image: url('{{ asset('app-assets/frontend/assets/images/slider/slider_home_first_3.jpg') }}');">
+            <div class="owl-slide d-flex align-items-center cover" style="background-image: url('{{ asset('app-assets/frontend/assets/images/slider/slider3.jpg') }}');">
                 <div class="container">
                     <div class="row justify-content-center justify-content-md-start no-gutters">
                         <div class="col-10 col-md-6 static">
                             <div class="owl-slide-text">
-                                <h3 class="owl-slide-animated owl-slide-title">Creating Impact</h3>
-                                <h1 class="owl-slide-animated owl-slide-subtitle">With Every Step We Take</h1>
+                                <h3 class="owl-slide-animated owl-slide-title bg-white text-dark p-3">IT DOESN’T MATTER IF YOU CANT SEE</h3>
+                                <h3 class="owl-slide-animated owl-slide-subtitle bg-dark p-3">WITH ANOTHER 200 CHILDREN LIKE YOU AT ALMUDASSAR LEARNING DEVELOPMENT CENTRE, YOU WILL ALWAYS FEEL LIKE FAMILY</h3>
                                 <div class="owl-slide-animated owl-slide-cta">
-                                    <a class="btn btn-default me-3" href="#" role="button">Make A Difference</a>
+                                    <a class="btn btn-default me-3" href="{{ route('learning_disability') }}" role="button">MORE DETAILS</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Slide 4 -->
+            <div class="owl-slide d-flex align-items-center cover" style="background-image: url('{{ asset('app-assets/frontend/assets/images/slider/slider4.jpg') }}');">
+                <div class="container">
+                    <div class="row justify-content-center justify-content-md-start no-gutters">
+                        <div class="col-10 col-md-6 static">
+                            <div class="owl-slide-text">
+                                <h3 class="owl-slide-animated owl-slide-title bg-white text-dark p-3">IT DOESN’T MATTER IF YOU DON'T UNDERSTAND</h3>
+                                <h3 class="owl-slide-animated owl-slide-subtitle bg-dark p-3">OUR SCHOOL FOR DEAF HAS 200 CHILDREN FROM KG CLASS TO 12TH GRADE SO COME AND REALIZE YOUR FULL POTENTIAL</h3>
+                                <div class="owl-slide-animated owl-slide-cta">
+                                    <a class="btn btn-default me-3" href="{{ route('school_for_deaf') }}" role="button">MORE DETAILS</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Slide 5 -->
+            <div class="owl-slide d-flex align-items-center cover" style="background-image: url('{{ asset('app-assets/frontend/assets/images/slider/slider5.jpg') }}');">
+                <div class="container">
+                    <div class="row justify-content-center justify-content-md-start no-gutters">
+                        <div class="col-10 col-md-6 static">
+                            <div class="owl-slide-text">
+                                <h3 class="owl-slide-animated owl-slide-title bg-white text-dark p-3">IT DOESN’T MATTER IF YOU ARE PHYSICALLY UNFIT</h3>
+                                <h3 class="owl-slide-animated owl-slide-subtitle bg-dark p-3">STRIVING TO MAKE LIFE EXCEPTIONAL FOR THE SPECIAL ONES. WE HAVE 40 CHILDREN IN ALMUDASSAR PHYSICAL TRAINING CENTRE</h3>
+                                <div class="owl-slide-animated owl-slide-cta">
+                                    <a class="btn btn-default me-3" href="{{ route('vocational_training') }}" role="button">MORE DETAILS</a>
                                 </div>
                             </div>
                         </div>
